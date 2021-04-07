ce563e41af7887a745a575bbc7ef569f6025ada0
hyemin <hhm930415@gmail.com>
2021-04-07 18:16:04 +0900

    2번로직

--- DemoForm2.py
+++ DemoForm2.py
@@ -1,18 +1,24 @@
-# 화면단(DemoForm.ui) + 로직단(DemoForm.py)
+# 화면단(DemoForm.ui) + 로직단(DemoForm2.py)
 import sys
 # Qt패키지 로딩 : 패키지명.모듈명
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 # 미리 만든 화면을 로딩
-form_class = uic.loadUiType("DemoForm.ui")[0]
+form_class = uic.loadUiType("DemoForm2.ui")[0]
 
-# 다이알로그를 상속받아서 폼클래스를 정의
-class DemoForm(QDialog, form_class):
+# 다이알로그를 상속받아서 폼클래스를 정의(QMainWindow)
+class DemoForm(QMainWindow, form_class):
     def __init__(self):
         super().__init__() # 아직 명시하지 못하므로 다 호출
         self.setupUi(self)
-        self.label.setText("첫번째 Qt데모")
+    # 시그널(이벤트)을 처리하는 슬롯메서드
+    def firstClick(self):
+        self.label.setText("첫번째 클릭")
+    def secondClick(self):
+        self.label.setText("두번째 버튼 클릭")
+    def thirdClick(self):
+        self.label.setText("3번 클릭클릭")
 
 # 직접 이 모듈을 실행했는지 진입점(Entry Point) 체크
 if __name__ == "__main__": # 이름이 맞는지 확인
@@ -22,5 +28,5 @@ if __name__ == "__main__": # 이름이 맞는지 확인
     demoWindow = DemoForm()
     # 창을 보여줌
     demoWindow.show()
-    # 지속적으로 실행 : 실행프로세스 실행. (이벤트 루프ㅡ=)
+    # 지속적으로 실행 : 실행프로세스 실행. (이벤트 루프)
     app.exec_() 
